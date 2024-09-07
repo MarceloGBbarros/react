b686febfe7eb0855ca7b4332db1973d2d588e61c
Marcelo Gonçlves <gbarros.marcelo@gmail.com>
2024-09-07 10:26:28 -0300

    atualizando projeto

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,16 @@
+//components
+import FirstComponent from "./components/FristComponent";
+
+//styles
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos react</h1>
+        <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
